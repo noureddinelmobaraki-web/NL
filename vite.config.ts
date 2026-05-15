e361bd373a3a60bdad1f54aeaaa9cc5c8e76f889
Noureddin El Mobaraki <noureddinelmobaraki@gmail.com>
2026-05-15 04:15:53 +0100

    Change base path from '/nradio/' to '/NL/'

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { compression } from 'vite-plugin-compression2';
 
 export default defineConfig(({ mode }) => {
   return {
-    base: '/nradio/',
+    base: '/NL/',
     plugins: [
       react(), 
       tailwindcss(),
